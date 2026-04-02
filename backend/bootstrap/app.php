9aa7e9600f8a9721d4f268024388a516bc21052f
maestrodev7 <youmbidefonam2@gmail.com>
2026-04-02 12:29:18 +0200

    Add CORS middleware to API request handling

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
             \App\Presentation\Http\Middleware\TrimStrings::class,
         ]);
     })
---
+++ backend/config/cors.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'https://135.125.226.143',
+        'https://135.125.226.143:4200',
+        'https://localhost:4200',
+        'http://localhost:4200',
+    ],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
+
