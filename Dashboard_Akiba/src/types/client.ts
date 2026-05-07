f973a1e2be06c04bc1276240f40d541a9b994d47
LORD_12 <fopawafo12@gmail.com>
2026-05-07 17:15:17 +0100

    modify payement, historique clent

--- Dashboard_Akiba/src/types/client.ts
+++ Dashboard_Akiba/src/types/client.ts
@@ -6,7 +6,59 @@ export interface Client {
   telephone: string;
   adresse: string;
   numero_registre: string | null;
+  simulation_payment_status: string;
+  simulation_paid_at: string | null;
 }
+
+
+
+
+adresse: "rue,45"
+
+
+
+
+created_at: "2026-05-05T09:52:00+00:00"
+
+
+
+
+email: "cod2camp@gmail.com"
+
+
+
+
+id: "ruwr1hzpruft"
+
+
+
+
+nom: "ERIC"
+
+
+
+
+numero_registre: null
+
+
+
+
+prenom: "Franck"
+
+
+
+
+simulation_paid_at: null
+
+
+
+
+
+
+
+
+
+telephone: "696618559"
 
 export interface ClientMeta {
   current_page: number;
